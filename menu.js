ae84496a4adeaf84a76522a9819bfe6112431c02
Leonardo Matteucci <leonardo.matteucci@icloud.com>
2025-08-08 23:07:21 +0200

    restore-hide-menu

--- menu.js
+++ menu.js
@@ -2,17 +2,10 @@ window.addEventListener('DOMContentLoaded', () => {
   const menuToggle = document.getElementById('menu-toggle');
   if (!menuToggle) return;
 
-  const isDesktop = window.matchMedia('(min-width: 601px)').matches;
-
-  if (isDesktop) {
-    menuToggle.checked = true;
-  } else {
-    document.addEventListener('click', (e) => {
-      if (!menuToggle.checked) return;
-      const header = document.querySelector('header');
-      if (!header.contains(e.target)) {
-        menuToggle.checked = false;
-      }
-    });
-  }
-});
+  document.addEventListener('click', (e) => {
+    if (!menuToggle.checked) return;
+    const header = document.querySelector('header');
+    if (!header.contains(e.target)) {
+      menuToggle.checked = false;
+    }
+  });
